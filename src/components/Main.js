43a1761232e7ba988fab552ffdda8fac47f6f213
sunami09 <sunamidasgupta@gmail.com>
2022-02-25 18:08:01 +0530

    Added My Initials - SD

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import LogoComponent from '../subComponents/LogoComponent'
 import PowerButton from '../subComponents/PowerButton'
 
 
@@ -28,6 +29,7 @@ const Main = () => {
     <MainContainer>
       <Container>
         <PowerButton />
+        <LogoComponent />
       </Container>
     </MainContainer>
   )
