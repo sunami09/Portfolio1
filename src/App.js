890223b30a6c06d1e99df2f1cea13adb556cee99
sunami09 <sunamidasgupta@gmail.com>
2022-02-25 17:21:27 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Route, Switch } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
+import Main from "./components/Main";
+import AboutPage from "./components/Main";
+import BlogPage from "./components/Main";
+import WorkPage from "./components/Main";
+import MySkillsPage from "./components/Main";
+
+
 function App() {
-  return <div>
-    App File
-    </div>
+  return <>
+
+  <GlobalStyle />
+    <ThemeProvider theme={lightTheme}>
+      <Switch>
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/" component={AboutPage}/>
+        <Route exact path="/" component={BlogPage}/>
+        <Route exact path="/" component={WorkPage} />
+        <Route exact path="/" component={MySkillsPage} />
+      </Switch>
+    </ThemeProvider>
+
+    </>
     
 }
 
