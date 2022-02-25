b5ad67f49454a4fde7082553e3cb769a6bd3cc5c
sunami09 <sunamidasgupta@gmail.com>
2022-02-25 18:31:49 +0530

    Added Contact feature (Get In touch)

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import LogoComponent from '../subComponents/LogoComponent'
 import PowerButton from '../subComponents/PowerButton'
 import SocialIcons from '../subComponents/SocialIcons'
+import { NavLink } from 'react-router-dom'
 
 
 const MainContainer = styled.div`
@@ -25,6 +26,15 @@ const Container = styled.div`
 padding: 2rem;
 `
 
+const Contact = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 2rem;
+right: calc(1rem + 2vw);
+text-decoration: none;
+z-index:1;
+`
+
 const Main = () => {
   return (
     <MainContainer>
@@ -32,6 +42,12 @@ const Main = () => {
         <PowerButton />
         <LogoComponent />
         <SocialIcons />
+
+        <Contact target="_blank" to={{pathname:"mailto:sunamidasgupta@gmail.com"}}>
+          <h3>
+            Say hi..
+          </h3>
+        </Contact>
       </Container>
     </MainContainer>
   )
